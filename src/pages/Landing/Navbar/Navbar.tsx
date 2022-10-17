7fb17986581f7b8af24ff23609a62236e25fd363
AhhYeaah <vivi26743@hotmail.com>
2022-10-17 16:08:30 -0300

    whiter navbar

--- src/pages/Landing/Navbar/Navbar.tsx
+++ src/pages/Landing/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { NavbarButtonGroup } from './NavbarButtonGroup/NavbarButtonGroup';
 
 export function Navbar() {
   return (
-    <header className="flex justify-center sticky top-0  w-full bg-slate-200">
+    <header className="flex justify-center sticky top-0 w-full bg-white">
       <section className="max-h-20 mx-auto p-4 px-7 container flex justify-between">
         <Link to={'/'} className="w-32">
           <img src="https://mrpg.app/assets/img/logo.png" />
