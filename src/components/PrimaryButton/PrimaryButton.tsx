18b2142b3a2a127d010d738c0cf08ea1f637e263
AhhYeaah <vivi26743@hotmail.com>
2022-10-29 02:35:11 -0300

    now buttons can do things!

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export function PrimaryButton(props: { children: any; className?: string }) {
+export function PrimaryButton(props: { children: any; onclick: () => any; className?: string }) {
   return (
     <button
       className={`bg-blue-600 rounded-md text-white hover:bg-blue-500 px-5 py-2 transition-colors duration-300 ${props.className}`}
+      onClick={props.onclick}
     >
       {props.children}
     </button>
