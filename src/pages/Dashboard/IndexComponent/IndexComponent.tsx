595c932948a9fd35bfdefd0c64f68dad47014fed
AhhYeaah <vivi26743@hotmail.com>
2022-10-31 23:55:02 -0300

    became internacional

--- src/pages/Dashboard/IndexComponent/IndexComponent.tsx
+++ src/pages/Dashboard/IndexComponent/IndexComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function IndexComponent() {
   return (
     <div className="h-full w-full bg-slate-400 flex-center">
-      <h1>Ola mundo</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
