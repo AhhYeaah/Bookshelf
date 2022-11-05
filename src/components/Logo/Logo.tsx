57fc66a8ff4e9a4eaf6009296bb3faa25c1630ba
AhhYeaah <vivi26743@hotmail.com>
2022-11-05 18:13:44 -0300

    added the possibility of having a dark mode logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
-import logoSrc from '../../assets/logo.png';
+import logoClaroSrc from '../../assets/logo.png';
+import logoDarkSrc from '../../assets/logo-claro.png';
 import miniLogoSrc from '../../assets/logo-pequeno.png';
 
 interface LogoProps {
   width?: number;
   className?: string;
   minified?: boolean;
+  dark?: boolean;
 }
 
-export function Logo({ width, className, minified }: LogoProps) {
-  return <img className={className} width={width} src={minified ?? false ? miniLogoSrc : logoSrc} alt="Logo do site" />;
+export function Logo({ width, className, minified, dark }: LogoProps) {
+  let logosrc;
+
+  if (minified) {
+    logosrc = miniLogoSrc;
+  } else {
+    if (dark) {
+      logosrc = logoDarkSrc;
+    } else {
+      logosrc = logoClaroSrc;
+    }
+  }
+  return <img className={className} width={width} src={logosrc} alt="Logo do site" />;
 }
