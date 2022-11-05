ad28b9a338226db7dda78b834c762351c5fa2d92
AhhYeaah <vivi26743@hotmail.com>
2022-11-05 18:15:18 -0300

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      "sidebar-bg": "#313a46",
+      "sidebar-item": "#8391a2",
+      "sidebar-item-hover": "#bccee4",
+      "sidebar-item-active": "#fff",
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
